210e2c40329a86c4ab04f351fafa33c2dcacf0d9
akshaybapat6365 <156952298+akshaybapat6365@users.noreply.github.com>
2025-05-28 15:30:15 -0600

    Add polygon bounce visualization

--- navigation.js
+++ navigation.js
@@ -61,9 +61,15 @@ function createSidebar() {
         <li class="nav-item ${window.location.pathname.includes('integration.html') ? 'active' : ''}">
           <a href="integration.html" class="nav-link">
             <svg width="20" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.14645 2.14645C7.34171 1.95118 7.65829 1.95118 7.85355 2.14645L11.8536 6.14645C12.0488 6.34171 12.0488 6.65829 11.8536 6.85355C11.6583 7.04882 11.3417 7.04882 11.1464 6.85355L8 3.70711L8 12.5C8 12.7761 7.77614 13 7.5 13C7.22386 13 7 12.7761 7 12.5L7 3.70711L3.85355 6.85355C3.65829 7.04882 3.34171 7.04882 3.14645 6.85355C2.95118 6.65829 2.95118 6.34171 3.14645 6.14645L7.14645 2.14645Z" fill="currentColor"></path></svg>
-            <span>Integration</span>
-          </a>
-        </li>
+          <span>Integration</span>
+        </a>
+      </li>
+      <li class="nav-item ${window.location.pathname.includes('hexagon.html') ? 'active' : ''}">
+        <a href="hexagon.html" class="nav-link">
+          <svg width="20" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.5 0L14 4L14 11L7.5 15L1 11L1 4L7.5 0Z" fill="currentColor"></path></svg>
+          <span>Polygon</span>
+        </a>
+      </li>
       </ul>
     </nav>
     <div class="sidebar-footer">
